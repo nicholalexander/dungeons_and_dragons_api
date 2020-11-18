f6720ac2e551b9136bf20b743f0b53d5abc89ebb
nichol alexander <nichol.alexander@gmail.com>
2020-11-18 09:01:16 -0500

    initial table

--- migrate/001_tables.rb
+++ migrate/001_tables.rb
@@ -1,7 +1,9 @@
 Sequel.migration do
   change do
-    create_table(:model1s) do
+    create_table(:monsters) do
       primary_key :id
+      String :name, null: false
+      Integer :hit_points, null: false
     end
   end
 end
